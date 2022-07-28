5ba7d2e53482360507dc4dfaefd11b0818edad77
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2022-07-28 19:01:05 +0044

    Obtendo um post por id

---
+++ src/main/java/com/franciscoclira/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.franciscoclira.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.franciscoclira.workshopmongo.domain.Post;
+import com.franciscoclira.workshopmongo.repository.PostRepository;
+import com.franciscoclira.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
