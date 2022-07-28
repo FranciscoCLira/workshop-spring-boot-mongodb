728418eef8fdf9853e1e93f7955261368ca57b61
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2022-07-28 09:18:18 +0044

    Criando entity Post com User aninhado

---
+++ src/main/java/com/franciscoclira/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.franciscoclira.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.franciscoclira.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
